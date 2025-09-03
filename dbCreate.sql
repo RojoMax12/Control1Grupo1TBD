b5b04c80ea0d0d87302a6a0801cf05cf80f0b74d
Johan <johan.neira@usach.cl>
2025-09-02 23:56:37 -0400

    Se modifico el LoadDatabase, el CreateDatabse y se añadio el  runStatements

--- dbCreate.sql
+++ dbCreate.sql
@@ -15,11 +15,6 @@ CREATE TABLE producto (
     precio NUMERIC(10,2) DEFAULT 0
 );
 
-CREATE TABLE sueldo (
-    id_sueldo INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    valor NUMERIC(10,2) DEFAULT 0,
-    mes VARCHAR(20) NOT NULL
-);
 
 CREATE TABLE comuna (
     id_comuna INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -33,45 +28,52 @@ CREATE TABLE tipodoc (
 
 CREATE TABLE empleado (
     id_empleado INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    id_sueldo INT,
     id_comuna INT,
     cargo VARCHAR(50) NOT NULL,
     nombre VARCHAR(100) NOT NULL,
-    FOREIGN KEY (id_sueldo) REFERENCES sueldo(id_sueldo),
     FOREIGN KEY (id_comuna) REFERENCES comuna(id_comuna)
+);
+
+CREATE TABLE sueldo (
+    id_sueldo INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id_empleado INT,
+    valor NUMERIC(10,2) DEFAULT 0,
+    mes VARCHAR(20) NOT NULL,
+    FOREIGN KEY (id_empleado) REFERENCES empleado(id_empleado)
 );
 
 CREATE TABLE vendedor (
 	id_vendedor INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	nombre VARCHAR(100) NOT NULL
+    id_empleado INT,
+    FOREIGN KEY (id_empleado) REFERENCES empleado(id_empleado)
 );
 
 CREATE TABLE tienda (
     id_tienda INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL
+    id_comuna INT,
+    nombre VARCHAR(100) NOT NULL,
+    FOREIGN KEY (id_comuna) REFERENCES comuna(id_comuna)
 );
 
 CREATE TABLE venta (
     id_venta INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     id_tienda INT,
-    id_vendedor INT,
     id_tipodoc INT,
-    dia INT,
-    año INT,
+    anio INT,
     mes VARCHAR(100),
     FOREIGN KEY (id_tienda) REFERENCES tienda(id_tienda),
-    FOREIGN KEY (id_vendedor) REFERENCES vendedor(id_vendedor),
     FOREIGN KEY (id_tipodoc) REFERENCES tipodoc(id_tipodoc)
 );
 
 CREATE TABLE producto_venta (
     id_producto INT,
     id_venta INT,
-    PRIMARY KEY (id_producto, id_venta),
+    id_vendedor INT,
+    PRIMARY KEY (id_producto, id_venta, id_vendedor),
     FOREIGN KEY (id_producto) REFERENCES producto(id_producto),
-    FOREIGN KEY (id_venta) REFERENCES venta(id_venta)
+    FOREIGN KEY (id_venta) REFERENCES venta(id_venta),
+    FOREIGN KEY (id_vendedor) REFERENCES vendedor(id_vendedor)
 );
-
 CREATE TABLE tienda_empleado (
     id_tienda INT,
     id_empleado INT,
@@ -80,10 +82,3 @@ CREATE TABLE tienda_empleado (
     FOREIGN KEY (id_empleado) REFERENCES empleado(id_empleado)
 );
 
-CREATE TABLE tienda_comuna (
-    id_tienda INT,
-    id_comuna INT,
-    PRIMARY KEY (id_tienda, id_comuna),
-    FOREIGN KEY (id_tienda) REFERENCES tienda(id_tienda),
-    FOREIGN KEY (id_comuna) REFERENCES comuna(id_comuna)
-);
